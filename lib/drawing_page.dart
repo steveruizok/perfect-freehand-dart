ef6b284b003e4331893fb9554dda07f38903fb0b
Rajesh <rajesh.k@technovert.com>
2021-10-01 10:26:48 +0530

    saving signature

--- lib/drawing_page.dart
+++ lib/drawing_page.dart
@@ -1,4 +1,7 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 
 import './perfect-freehand/stroke.dart';
 import "./perfect-freehand/point.dart";
@@ -18,8 +21,11 @@ class _DrawingPageState extends State<DrawingPage> {
   List<Stroke> lines = <Stroke>[];
 
   Stroke? line;
+  ui.PictureRecorder recorder = ui.PictureRecorder();
 
   StrokeOptions options = StrokeOptions();
+  Canvas? canvas;
+  Sketcher? sketcher;
 
   StreamController<Stroke> currentLineStreamController =
       StreamController<Stroke>.broadcast();
@@ -27,10 +33,26 @@ class _DrawingPageState extends State<DrawingPage> {
   StreamController<List<Stroke>> linesStreamController =
       StreamController<List<Stroke>>.broadcast();
 
+  Uint8List? imageData;
+  bool _isPaintStarted = false;
+
+  set isPaintStarted(bool isStarted) {
+     setState(() {
+       _isPaintStarted = isStarted;
+     });
+  }
+
+  @override
+  void initState() {
+    canvas = Canvas(recorder);
+    super.initState();
+  }
+
   Future<void> clear() async {
     setState(() {
       lines = [];
       line = null;
+      isPaintStarted = false;
     });
   }
 
@@ -41,6 +63,7 @@ class _DrawingPageState extends State<DrawingPage> {
   }
 
   void onPanStart(DragStartDetails details) {
+    isPaintStarted = true;
     final box = context.findRenderObject() as RenderBox;
     final offset = box.globalToLocal(details.globalPosition);
     final point = Point(offset.dx, offset.dy);
@@ -61,6 +84,59 @@ class _DrawingPageState extends State<DrawingPage> {
   void onPanEnd(DragEndDetails details) {
     lines = List.from(lines)..add(line!);
     linesStreamController.add(lines);
+  }
+
+  Future<ui.Image?> toImage() async {
+    sketcher = Sketcher(lines: lines, options: options);
+    if (lines.isEmpty) {
+      return null;
+    }
+
+    double minX = double.infinity, minY = double.infinity;
+    double maxX = 0, maxY = 0;
+    for (int i = 0; i < lines.length; ++i) {
+      var line = lines[i];
+      for (Point point in line.path) {
+        if (point.x < minX) {
+          minX = point.x;
+        }
+        if (point.y < minY) {
+          minY = point.y;
+        }
+        if (point.x > maxX) {
+          maxX = point.x;
+        }
+        if (point.y > maxY) {
+          maxY = point.y;
+        }
+      }
+    }
+
+    final ui.PictureRecorder recorder = ui.PictureRecorder();
+    final ui.Canvas canvas = Canvas(recorder)
+      ..translate(-(minX - options.size), -(minY - options.size));
+    final ui.Paint paint = Paint()..color = Colors.transparent;
+    canvas.drawPaint(paint);
+
+    sketcher?.paint(canvas, Size.infinite);
+    final ui.Picture picture = recorder.endRecording();
+    return picture.toImage(
+      (maxX - minX + options.size * 2).toInt(),
+      (maxY - minY + options.size * 2).toInt(),
+    );
+  }
+
+  saveSignature() async {
+    if (canvas != null) {
+      ui.Image? image = await toImage();
+      ByteData? byteData =
+          await image?.toByteData(format: ui.ImageByteFormat.png);
+      if (byteData != null) {
+        setState(() {
+          imageData = byteData.buffer.asUint8List();
+        });
+      }
+    }
   }
 
   Widget buildCurrentPath(BuildContext context) {
@@ -224,7 +300,23 @@ class _DrawingPageState extends State<DrawingPage> {
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
               ),
               buildClearButton(),
-              
+              (_isPaintStarted)
+                  ? ElevatedButton(
+                      onPressed: () async {
+                        await saveSignature();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Scaffold(
+                                      body: Center(
+                                        child: Image.memory(imageData ??
+                                            Uint8List.fromList([])),
+                                      ),
+                                    )));
+                        clear();
+                      },
+                      child: const Text("Save"))
+                  : Container(),
             ]));
   }
 
@@ -232,9 +324,9 @@ class _DrawingPageState extends State<DrawingPage> {
     return GestureDetector(
       onTap: clear,
       child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: const CircleAvatar(
-        child: Icon(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: const CircleAvatar(
+            child: Icon(
           Icons.replay,
           size: 20.0,
           color: Colors.white,
