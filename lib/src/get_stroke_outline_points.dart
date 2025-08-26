428ff19c51f25c98634b5f00c1a13aa408cda5fb
dodgog <137945767+dodgog@users.noreply.github.com>
2025-08-26 20:42:07 +0100

    Make the end cap a half-circle instead of a full circle (#22)
    * Make the end cap a half-circle instead of a full
    
    * tweak: align better with existing code
    
    * chore: update goldens for new end caps
    
    ---------
    
    Co-Authored-By: adil192 <adilhanney@disroot.org>

--- lib/src/get_stroke_outline_points.dart
+++ lib/src/get_stroke_outline_points.dart
@@ -345,9 +345,9 @@ List<Offset> getStrokeOutlinePoints(
   } else if (options.end.cap) {
     // Draw the round end cap
     final start = lastPoint.project(direction, radius);
-    const step = 1 / 29;
+    const step = 1 / 13;
     for (double t = step; t <= 1; t += step) {
-      endCap.add(start.rotAround(lastPoint, fixedPi * 3 * t));
+      endCap.add(start.rotAround(lastPoint, fixedPi * t));
     }
   } else {
     // Draw the flat end cap
